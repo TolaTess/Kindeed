0ea713bd67ab137d8dc88a3bdb69d6f81fe8bbe8
Tola Tess <x17131553@student.ncirl.ie>
2020-06-13 11:23:24 +0100

    Splash Activities
    added intiail splash activities

---
+++ app/src/main/java/com/tolaotesanya/kindeed/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.tolaotesanya.kindeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 2000;
+
+    Animation animation;
+    TextView app_title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.splash_anim);
+        app_title = findViewById(R.id.app_name);
+
+        app_title.setAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(splashIntent);
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
